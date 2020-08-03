cf88ed985efd3260109776e205fa8e05476ebc01
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-08-03 17:34:18 +0200

    chore: added required .page suffix for Next page matching

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -15,15 +15,15 @@ export default class MyDocument extends Document {
         })
 
       const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
+
+      const styles = (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      )
+
+      return { ...initialProps, styles }
     } finally {
       sheet.seal()
     }
